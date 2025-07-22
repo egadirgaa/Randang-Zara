63e8468614f3475cb73c3ea807794ef690d86d28
egadirgaa <nesza.anes89@gmail.com>
2025-07-22 13:36:03 +0700

    randang zara

---
+++ script.js
@@ -0,0 +1,30 @@
+// Function menu mobile
+const menuBtn = document.getElementById('menu-btn');
+const mobileMenu = document.getElementById('mobile-menu');
+menuBtn.addEventListener('click', () => {
+    menuBtn.classList.toggle('active');
+    mobileMenu.classList.toggle('hidden');
+    mobileMenu.classList.toggle('max-h-0');
+    mobileMenu.classList.toggle('max-h-screen');
+});
+// Close menu when a link is clicked Tutup klik
+const navLinks = document.querySelectorAll('#mobile-menu a');
+navLinks.forEach(link => {
+    link.addEventListener('click', () => {
+        menuBtn.classList.remove('active');
+        mobileMenu.classList.add('hidden');
+        mobileMenu.classList.add('max-h-0');
+        mobileMenu.classList.remove('max-h-screen');
+    });
+});
+// Shadow Sewaktu Scroll
+window.addEventListener('scroll', () => {
+    const header = document.querySelector('header');
+    if (window.scrollY > 10) {
+        header.classList.add('shadow-md');
+        header.classList.remove('shadow-sm');
+    } else {
+        header.classList.remove('shadow-md');
+        header.classList.add('shadow-sm');
+    }
+});
